9bebe9052b19b30f81e6cde0820dbf2974e9035b
terry-xiaoyu <506895667@qq.com>
2020-07-23 17:08:46 +0800

    feature(mod_vsn): transform vsn attr using environment variable

--- src/mod_vsn.erl
+++ src/mod_vsn.erl
@@ -3,5 +3,15 @@
 -export([parse_transform/2]).
 
 parse_transform(AST, _Opts) ->
-    io:format("----mod vsn--~n"),
-    AST.
+    case os:getenv("BUILD_VERSION") of
+        false -> io:format("--- BUILD_VERSION not found ~n", []), AST;
+        Vsn -> trans(Vsn, AST)
+    end.
+
+trans(Vsn, AST) ->
+    lists:map(
+        fun ({attribute, L, vsn, _OldVsn}) ->
+                io:format("--- changing vsn from ~p to ~p~n", [_OldVsn, Vsn]),
+                {attribute, L, vsn, Vsn};
+            (Token) -> Token
+        end, AST).
