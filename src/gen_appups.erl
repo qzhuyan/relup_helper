bd11d5c4c714796f599f94b3233e1a32a0f7377a
terry-xiaoyu <506895667@qq.com>
2020-07-28 19:31:42 +0800

    fix(gen_appup): unzip failure

--- src/gen_appups.erl
+++ src/gen_appups.erl
@@ -8,6 +8,7 @@ do(State) ->
     gen_appups(rebar_dir:base_dir(State)).
 
 gen_appups(BaseDir) ->
+    {ok, CWD} = file:get_cwd(),
     [begin
         AppName = filename:basename(Dir),
         AppFile = filename:join([Dir, "ebin", AppName++".app"]),
@@ -32,8 +33,8 @@ gen_appups(BaseDir) ->
                 write_file(AppupFile, AppupText)
         end
      end || Dir <- filelib:wildcard(filename:join([BaseDir, "lib", "*"])), filelib:is_dir(Dir)],
-     untar_and_copy(tar, "*.tar.gz", BaseDir),
-     untar_and_copy(zip, "*.zip", BaseDir).
+     untar_and_copy(tar, filename:join([CWD, "*.tar.gz"]), BaseDir),
+     untar_and_copy(zip, filename:join([CWD, "*.zip"]), BaseDir).
 
 write_file(Filename, Text) ->
     io:format("writing to file: ~p~n", [Filename]),
@@ -41,24 +42,24 @@ write_file(Filename, Text) ->
 
 untar_and_copy(Type, Pattern, BaseDir) ->
     [begin
-        TmpDir = filename:join(["tmp", "emqx_untar", integer_to_list(erlang:system_time())]),
-        ok = file:make_dir(TmpDir),
+        TmpDir = filename:join(["/tmp", "emqx_untar", integer_to_list(erlang:system_time())]),
+        ok = filelib:ensure_dir(filename:join([TmpDir, "dummy"])),
         untar_pre_vsn_packages(Type, Ball, TmpDir),
         copy_dirs([TmpDir, "emqx", "lib", "*"], [BaseDir, "rel", "emqx", "lib"]),
         copy_dirs([TmpDir, "emqx", "releases", "*"], [BaseDir, "rel", "emqx", "releases"]),
         file:del_dir(TmpDir)
-     end || Ball <- filelib:wildcard([Pattern]), filelib:is_file(Ball)].
+     end || Ball <- filelib:wildcard(Pattern), filelib:is_file(Ball)].
 
 copy_dirs(SrcPath, DstPath) ->
     SrcPath0 = filename:join(SrcPath),
     [begin
-        DirName = file:basename(SrcPath0),
+        DirName = filename:basename(PrevD),
         DstPath0 = filename:join(DstPath ++ [DirName]),
         case filelib:is_dir(DstPath0) of
             true -> %% already exists
                 skip;
             false ->
-                do_copy(SrcPath0, DstPath0)
+                do_copy(PrevD, DstPath0)
         end
      end|| PrevD <- filelib:wildcard(SrcPath0), filelib:is_dir(PrevD)].
 
