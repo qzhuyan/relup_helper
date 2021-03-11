557608b6e5ea8ebf0befba386dd7a5e7ddcb9fae
Zaiming Shi <zmstone@gmail.com>
2021-03-11 13:00:37 +0100

    chore(src): Delete unused modules
    `mod_vsn` parse_transform has been replaced with compile_info module
    attributes which is injected at comile time for ALL modules compiled
    with in emqx project
    
    `relup_helper_otp_vsn` has been replaced by BULT_ON relx template

--- src/relup_helper.erl
+++ src/relup_helper.erl
@@ -5,6 +5,4 @@
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
     {ok, State0} = relup_helper_gen_appups:init(State),
-    {ok, State1} = relup_helper_untar:init(State0),
-    {ok, State2} = relup_helper_otp_vsn:init(State1),
-    {ok, State2}.
+    {ok, _}  = relup_helper_untar:init(State0).
